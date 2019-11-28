7eb524e2fbba665316b5a43446e58470728cf60b
chanti <govardhan453@gmail.com>
2019-11-28 16:15:17 +0530

    wevgr

--- src/main/java/com/wd/utils/Demo.java
+++ src/main/java/com/wd/utils/Demo.java
@@ -10,6 +10,8 @@ System.out.println("Demo2");
 		System.out.println("Demo3");
 		System.out.println("changed in remote repository");
 		System.out.println("hello");
+		System.out.println("ravi");
+		System.out.println("veera");
 	
 	}
 
